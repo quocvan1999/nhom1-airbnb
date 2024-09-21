3ac19f0e0832e84cdd4d387a252205dd56611a8e
Quocvan <lyquocvan1999@gmail.com>
2024-09-21 20:05:41 +0700

    cap nhat code header lan 4

--- src/components/header-nav/HeaderNav.tsx
+++ src/components/header-nav/HeaderNav.tsx
@@ -1,7 +1,19 @@
+import { Dropdown, MenuProps } from "antd";
 import Link from "next/link";
 import React from "react";
 
 type Props = {};
+
+const items: MenuProps["items"] = [
+  {
+    key: "1",
+    label: <Link href="/">Đăng nhập</Link>,
+  },
+  {
+    key: "2",
+    label: <Link href="/">Đăng xuất</Link>,
+  },
+];
 
 const HeaderNav: React.FC<Props> = ({}) => {
   return (
@@ -25,26 +37,28 @@ const HeaderNav: React.FC<Props> = ({}) => {
         </svg>
       </div>
 
-      <div className="border flex items-center gap-2 px-3 py-1 rounded-full cursor-pointer transition-all duration-500 ease-in-out hover:shadow-md">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          viewBox="0 0 24 24"
-          style={{ fill: "#222" }}
-        >
-          <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path>
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="35"
-          height="35"
-          viewBox="0 0 24 24"
-          style={{ fill: "#222" }}
-        >
-          <path d="M12 2C6.579 2 2 6.579 2 12s4.579 10 10 10 10-4.579 10-10S17.421 2 12 2zm0 5c1.727 0 3 1.272 3 3s-1.273 3-3 3c-1.726 0-3-1.272-3-3s1.274-3 3-3zm-5.106 9.772c.897-1.32 2.393-2.2 4.106-2.2h2c1.714 0 3.209.88 4.106 2.2C15.828 18.14 14.015 19 12 19s-3.828-.86-5.106-2.228z"></path>
-        </svg>
-      </div>
+      <Dropdown menu={{ items }} placement="bottom" trigger={["click"]}>
+        <div className="border flex items-center gap-2 px-3 py-1 rounded-full cursor-pointer transition-all duration-500 ease-in-out hover:shadow-md">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            style={{ fill: "#222" }}
+          >
+            <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path>
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="35"
+            height="35"
+            viewBox="0 0 24 24"
+            style={{ fill: "#222" }}
+          >
+            <path d="M12 2C6.579 2 2 6.579 2 12s4.579 10 10 10 10-4.579 10-10S17.421 2 12 2zm0 5c1.727 0 3 1.272 3 3s-1.273 3-3 3c-1.726 0-3-1.272-3-3s1.274-3 3-3zm-5.106 9.772c.897-1.32 2.393-2.2 4.106-2.2h2c1.714 0 3.209.88 4.106 2.2C15.828 18.14 14.015 19 12 19s-3.828-.86-5.106-2.228z"></path>
+          </svg>
+        </div>
+      </Dropdown>
     </div>
   );
 };
