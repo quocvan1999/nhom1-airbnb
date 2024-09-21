d40a078dd2e3359efde045dd483c24e2f28380be
Quocvan <lyquocvan1999@gmail.com>
2024-09-21 20:12:46 +0700

    cap nhat code header lan 5

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,21 @@
+"use client";
+
 import HeaderNav from "@/components/header-nav/HeaderNav";
 import HeaderSearch from "@/components/header-search/HeaderSearch";
+import useStatusHeader from "@/custome-hook/useStatusHeader/useStatusHeader";
 import React from "react";
 
 type Props = {};
 
 const Header: React.FC<Props> = ({}) => {
+  const { isScroll } = useStatusHeader();
   return (
     <div className="w-full border-b">
-      <div className="max-w-[1280px] px-2 mx-auto flex items-center justify-between py-3">
+      <div
+        className={`max-w-[1280px] px-2 mx-auto flex items-center justify-between py-3 transition-all duration-500 ease-in-out ${
+          isScroll === false && "pb-[90px]"
+        }`}
+      >
         <div className="w-[110px]">
           <img src="/images/Airbnb_Logo.png" alt="logo" />
         </div>
