11bf7a05c135a7e8918edf9b1be1ec4565c05bd3
Quocvan <lyquocvan1999@gmail.com>
2024-09-28 20:10:12 +0700

    cap nhat avatar

--- src/services/avatar/avatar.service.ts
+++
@@ -1,6 +0,0 @@
-import { httpClient } from "@/utils/setting/setting";
-
-export const uploadAvatarAsync = async (formData: any) => {
-  const res = await httpClient.post("/api/users/upload-avatar", formData);
-  return res.data;
-};
