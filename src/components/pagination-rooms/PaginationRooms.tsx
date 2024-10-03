96ad2d9aada324eb8486efacfec41503442434c5
Quocvan <lyquocvan1999@gmail.com>
2024-10-03 15:56:13 +0700

    cap nhat pagination home 2

--- src/components/pagination-rooms/PaginationRooms.tsx
+++ src/components/pagination-rooms/PaginationRooms.tsx
@@ -1,42 +1,10 @@
-// "use client";
-
-// import { reqPaginationType } from "@/types/req-pagination/reqPaginationType.type";
-// import { RoomType } from "@/types/room/roomType.type";
-// import { Pagination } from "antd";
-// import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
-// import { useRouter } from "next/navigation";
-// import React from "react";
-
-// type Props = {
-//   data: reqPaginationType<RoomType[]>;
-// };
-
-// const PaginationRooms: React.FC<Props> = ({ data }) => {
-//   const router: AppRouterInstance = useRouter();
-//   const handleChangePageIndex = (page: number, pageSize: number): void => {
-//     router.push(`/?page=${page}&size=${pageSize}`);
-//   };
-
-//   return (
-//     <Pagination
-//       align="end"
-//       defaultCurrent={1}
-//       current={data.pageIndex}
-//       defaultPageSize={10}
-//       pageSize={data.pageSize}
-//       total={data.totalRow}
-//       onChange={handleChangePageIndex}
-//     />
-//   );
-// };
-
-// export default PaginationRooms;
 "use client";
 
 import { reqPaginationType } from "@/types/req-pagination/reqPaginationType.type";
 import { RoomType } from "@/types/room/roomType.type";
 import { Pagination } from "antd";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React from "react";
 
@@ -62,7 +30,9 @@ const PaginationRooms: React.FC<Props> = ({ data }) => {
       onChange={handleChangePageIndex}
       itemRender={(page, type, originalElement) => {
         if (type === "page") {
-          return <a href={`/?page=${page}&size=${data.pageSize}`}>{page}</a>;
+          return (
+            <Link href={`/?page=${page}&size=${data.pageSize}`}>{page}</Link>
+          );
         }
         return originalElement;
       }}
