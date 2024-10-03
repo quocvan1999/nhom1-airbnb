b33ec04d574e71591152325cd7570db447629022
Quocvan <lyquocvan1999@gmail.com>
2024-10-03 15:50:02 +0700

    cap nhat pagination home 1

--- src/components/pagination-rooms/PaginationRooms.tsx
+++ src/components/pagination-rooms/PaginationRooms.tsx
@@ -1,3 +1,36 @@
+// "use client";
+
+// import { reqPaginationType } from "@/types/req-pagination/reqPaginationType.type";
+// import { RoomType } from "@/types/room/roomType.type";
+// import { Pagination } from "antd";
+// import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+// import { useRouter } from "next/navigation";
+// import React from "react";
+
+// type Props = {
+//   data: reqPaginationType<RoomType[]>;
+// };
+
+// const PaginationRooms: React.FC<Props> = ({ data }) => {
+//   const router: AppRouterInstance = useRouter();
+//   const handleChangePageIndex = (page: number, pageSize: number): void => {
+//     router.push(`/?page=${page}&size=${pageSize}`);
+//   };
+
+//   return (
+//     <Pagination
+//       align="end"
+//       defaultCurrent={1}
+//       current={data.pageIndex}
+//       defaultPageSize={10}
+//       pageSize={data.pageSize}
+//       total={data.totalRow}
+//       onChange={handleChangePageIndex}
+//     />
+//   );
+// };
+
+// export default PaginationRooms;
 "use client";
 
 import { reqPaginationType } from "@/types/req-pagination/reqPaginationType.type";
@@ -13,6 +46,7 @@ type Props = {
 
 const PaginationRooms: React.FC<Props> = ({ data }) => {
   const router: AppRouterInstance = useRouter();
+
   const handleChangePageIndex = (page: number, pageSize: number): void => {
     router.push(`/?page=${page}&size=${pageSize}`);
   };
@@ -26,6 +60,12 @@ const PaginationRooms: React.FC<Props> = ({ data }) => {
       pageSize={data.pageSize}
       total={data.totalRow}
       onChange={handleChangePageIndex}
+      itemRender={(page, type, originalElement) => {
+        if (type === "page") {
+          return <a href={`/?page=${page}&size=${data.pageSize}`}>{page}</a>;
+        }
+        return originalElement;
+      }}
     />
   );
 };
