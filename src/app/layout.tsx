ff0fd53b6032cfd64c90dbc760cd2adc528ed8e3
Quocvan <lyquocvan1999@gmail.com>
2024-10-03 11:55:06 +0700

    cap nhat metadata 5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="canonical" href="https://nhom1-airbnb.vercel.app" />
+      </head>
       <body className={`font-custom text-[14px] text-custome-black-100`}>
         <Providers>
           <AntdRegistry>
