126fbdc724af77c75d5f657970da4b7a401eb6c7
Quocvan <lyquocvan1999@gmail.com>
2024-10-05 10:40:45 +0700

    reponsive form register 2

--- src/app/(client)/(auth)/auth/layout.tsx
+++ src/app/(client)/(auth)/auth/layout.tsx
@@ -9,7 +9,7 @@ export default function AuthLayout({
     <div className="w-full min-h-[100vh] bg-gray-100 flex items-center justify-center">
       {/* className="w-[900px] h-[80%] bg-white rounded-2xl shadow-xl flex" */}
       <div
-        className="w-full md:max-w-[70%] lg:max-w-[900px] mx-3 md:mx-0 bg-white rounded-xl shadow-xl flex"
+        className="w-full my-5 md:my-0 md:max-w-[70%] lg:max-w-[900px] mx-3 md:mx-0 bg-white rounded-xl shadow-xl flex"
         style={{
           backgroundImage:
             "linear-gradient(to left bottom, #ff385c, #fd3559, #fb3255, #f82e52, #f62b4f)",
