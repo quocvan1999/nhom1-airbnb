8848276d46609291dd082d9f4d362de328d48a43
Quocvan <lyquocvan1999@gmail.com>
2024-10-20 04:13:06 +0700

    cap nhat loading data trang nguoi dung 1

--- src/app/(admin)/admin/(admin)/page.tsx
+++ src/app/(admin)/admin/(admin)/page.tsx
@@ -284,7 +284,7 @@ const AdminPage: React.FC<Props> = ({ searchParams }) => {
 
   useEffect(() => {
     getData();
-  }, [isLoading]);
+  }, [searchParams.page, searchParams.size, isLoading]);
 
   return (
     <>
@@ -330,7 +330,6 @@ const AdminPage: React.FC<Props> = ({ searchParams }) => {
                   total={users?.totalRow}
                   onChange={(page: number, pageSize: number): void => {
                     router.push(`/admin/?page=${page}&size=${pageSize}`);
-                    setIsLoading(!isLoading);
                   }}
                 />
               )}
