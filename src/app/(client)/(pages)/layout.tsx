844fdb5446294f8f81f477d96c5b7edaaedf04de
Quocvan <lyquocvan1999@gmail.com>
2024-10-25 08:17:34 +0700

    sua mau pagination client

--- src/app/(client)/(pages)/layout.tsx
+++ src/app/(client)/(pages)/layout.tsx
@@ -2,6 +2,7 @@ import Footer from "@/components/footer/Footer";
 import HeaderMobile from "@/components/header-mobile/HeaderMobile";
 import Header from "@/components/header/Header";
 import NavMobile from "@/components/nav-mobile/NavMobile";
+import { ConfigProvider } from "antd";
 
 export default function ClientLayout({
   children,
@@ -9,20 +10,34 @@ export default function ClientLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="w-full">
-      <div className="w-full fixed top-0 left-0 right-0 bg-white z-[200]">
-        <Header />
-        <HeaderMobile />
+    <ConfigProvider
+      theme={{
+        components: {
+          Pagination: {
+            colorPrimary: "#FF385C",
+          },
+        },
+        token: {
+          colorPrimary: "#FF385C",
+          colorLink: "#FF385C",
+        },
+      }}
+    >
+      <div className="w-full">
+        <div className="w-full fixed top-0 left-0 right-0 bg-white z-[200]">
+          <Header />
+          <HeaderMobile />
+        </div>
+        <div className="block mdc:hidden w-full fixed bottom-0 left-0 right-0 bg-white z-[200]">
+          <NavMobile />
+        </div>
+        <div className="max-w-[1280px] mx-auto px-2 pt-[100px] mdc:pt-[160px] pb-[50px]">
+          {children}
+        </div>
+        <div className="w-full bg-custome-gray-100">
+          <Footer />
+        </div>
       </div>
-      <div className="block mdc:hidden w-full fixed bottom-0 left-0 right-0 bg-white z-[200]">
-        <NavMobile />
-      </div>
-      <div className="max-w-[1280px] mx-auto px-2 pt-[100px] mdc:pt-[160px] pb-[50px]">
-        {children}
-      </div>
-      <div className="w-full bg-custome-gray-100">
-        <Footer />
-      </div>
-    </div>
+    </ConfigProvider>
   );
 }
