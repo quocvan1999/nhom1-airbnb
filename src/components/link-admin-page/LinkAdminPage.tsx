de3acb8d0a7f0d548855860b7706cdd5dd3abf1f
Quocvan <lyquocvan1999@gmail.com>
2024-11-09 13:18:59 +0700

    update middleware admin page 2

--- src/components/link-admin-page/LinkAdminPage.tsx
+++ src/components/link-admin-page/LinkAdminPage.tsx
@@ -4,13 +4,16 @@ import { RootState } from "@/app/globalRedux/store";
 import useCheckLogin from "@/custome-hook/useCheckLogin/useCheckLogin";
 import useGetProfile from "@/custome-hook/useGetProfile/useGetProfile";
 import { setCookie } from "@/utils/method/method";
+import { Button } from "antd";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 type Props = {};
 
 const LinkAdminPage: React.FC<Props> = ({}) => {
+  const router = useRouter();
   const { checkIsLogin } = useCheckLogin();
   const { getProfile } = useGetProfile();
   const [isShow, setIsShow] = useState<boolean>(false);
@@ -37,19 +40,19 @@ const LinkAdminPage: React.FC<Props> = ({}) => {
   return (
     <>
       {isShow && (
-        <Link
+        <Button
           onClick={() => {
             setCookie(
               "auth_a",
               btoa(`${process.env.NEXT_PUBLIC_ADMIN_TOKEN}`),
               0
             );
+            router.push("/admin");
           }}
-          href="/admin"
-          className="font-medium text-custome-black-100 px-5 py-2 rounded-full cursor-pointer transition-all duration-500 ease-in-out hover:bg-custome-gray-100"
+          className="!border-none !shadow-none !rounded-full hover:!bg-[#b1b0b01e] !px-7 !text-sm !font-semibold !py-5 !transition-all !duration-500 !ease-in-out"
         >
           Đi đến trang quản trị
-        </Link>
+        </Button>
       )}
     </>
   );
