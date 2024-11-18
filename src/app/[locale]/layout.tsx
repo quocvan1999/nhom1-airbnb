288eb88f9a44839d4649ef560597edd631aaabc9
Quocvan <lyquocvan1999@gmail.com>
2024-11-19 01:05:48 +0700

    config next-intl 3

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,26 +2,34 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { Providers } from "@/app/[locale]/globalRedux/provider";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
 import "./globals.css";
 
 config.autoAddCss = false;
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
+  params: { locale },
 }: Readonly<{
   children: React.ReactNode;
+  params: { locale: string };
 }>) {
+  const messages = await getMessages();
+
   return (
-    <html lang="en">
+    <html lang={locale}>
       <head>
         <link rel="canonical" href="https://nhom1-airbnb.vercel.app" />
       </head>
       <body className={`font-custom text-[14px] text-custome-black-100`}>
-        <Providers>
-          <AntdRegistry>
-            <div className="w-full">{children}</div>
-          </AntdRegistry>
-        </Providers>
+        <NextIntlClientProvider messages={messages}>
+          <Providers>
+            <AntdRegistry>
+              <div className="w-full">{children}</div>
+            </AntdRegistry>
+          </Providers>
+        </NextIntlClientProvider>
       </body>
     </html>
   );
